1cd974c1b7d697aac04e5db399249bd484c7ee9b
Abdulquddus Ibrahim <ibrahimabdulquddus51@gmail.com>
2024-01-09 19:27:23 +0100

    add details of mongodb

--- app.js
+++ app.js
@@ -8,12 +8,33 @@ const app = express();
 app.use(bodyParser.json());
 
 // Connect to MongoDB
-const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://geocoding:geocoding123@<cluster>.mongodb.net/restapi?retryWrites=true&w=majority', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://GeoApi:GeoApi1234@restapi.wstlbei.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 // Routes
 const dataRoutes = require('./routes/dataRoute');
